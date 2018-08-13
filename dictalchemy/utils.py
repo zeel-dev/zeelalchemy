136020270f7436d9f38dee039afaadce0fcfbba9
Jasmine Chan <jasmine.chan@zeel.com>
2018-08-13 10:54:42 -0400

    Add changes to go another level deeper in asdict

--- dictalchemy/utils.py
+++ dictalchemy/utils.py
@@ -136,7 +136,22 @@ def asdict(model, exclude=None, exclude_underscore=None, exclude_pk=None,
 
             for child in rel:
                 if hasattr(child, method):
-                    rel_data.append(getattr(child, method)(**args))
+                    child_dict = getattr(child, method)(**args)
+                    children = args.pop('children', None)
+                    if children is not None:
+                        for key in children:
+                            rel_child = getattr(child, key)
+                            if isinstance(rel_child, list):
+                                rel_child_list = []
+                                for grandchild in rel_child:
+                                    rel_child_list.append(
+                                        getattr(grandchild, method)(**args))
+                                child_dict[key] = rel_child_list
+                            else:
+                                child_dict[key] = getattr(rel_child,
+                                                          method)(**args)
+
+                    rel_data.append(child_dict)
                 else:
                     try:
                         rel_data.append(dict(child))
