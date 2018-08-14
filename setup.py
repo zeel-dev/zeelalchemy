a3355a8a5533d2bf7383b2d3f33ca363d6d65976
Julian Raymar <julian.raymar@zeel.com>
2018-08-14 17:01:09 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if sys.version_info >= (3,):
     extra['use_2to3'] = True
 
 setup(name='zeelalchemy',
-      version='0.1.2.7',
+      version='0.1.2.8',
       description="Contains asdict and fromdict methods for SQL-Alchemy "
       "declarative models",
       long_description=README,
